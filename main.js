cd3e3821b941ddf094fdc1c851bd4edd75fe28ae
Light Code <86146814+lightcodecamp@users.noreply.github.com>
2024-07-16 12:19:47 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/*~~~~~~~~~~~~~~~ NAVBAR ~~~~~~~~~~~~~~~*/ 
+
+// active link
+
+    // sticky navbar
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/ 
+
+/*~~~~~~~~~~~~~~~ ANIMATION ~~~~~~~~~~~~~~~*/ 
+
+/*~~~~~~~~~~~~~~~ TYPED JS ~~~~~~~~~~~~~~~*/ 
