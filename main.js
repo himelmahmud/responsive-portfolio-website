48964b3a693b265fa0e167d855b1902bba6e8388
Himel Mahmud <91639059+himelmahmud@users.noreply.github.com>
2024-10-08 03:57:52 +0600

    responsive done

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 /*~~~~~~~~~~~~~~~ NAVBAR ~~~~~~~~~~~~~~~*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("ri-close-large-line");
+  navbar.classList.toggle("active");
+});
+
+// active link
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onsecroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // sticky navbar
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and navbar when click when click navbar link (scroll)
+  menuIcon.classList.remove("ri-close-large-line");
+  navbar.classList.remove("active");
+};
 // active link
 
 // sticky navbar
@@ -7,6 +43,13 @@
 // remove toggle icon and navbar when click navbar link (scroll)
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*~~~~~~~~~~~~~~~ ANIMATION ~~~~~~~~~~~~~~~*/
 
